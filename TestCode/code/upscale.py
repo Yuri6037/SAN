e1647e71a0f2577db3052b44167511416df4c218
Yuri Edward <yuri6037@outlook.com>
2021-10-22 17:45:16 +0200

    Added print_model property

--- TestCode/code/upscale.py
+++ TestCode/code/upscale.py
@@ -30,6 +30,7 @@ class PregenArgs:
         self.reduction = 16
         self.n_colors = 3
         self.res_scale = 1
+        self.print_model = True
 
 def prepare(args, l):
     device = torch.device('cpu' if args.cpu else 'cuda')
