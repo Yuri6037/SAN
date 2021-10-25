fcb9e34ac8db96bf3c85541ee253cd084b339de1
Yuri Edward <yuri6037@outlook.com>
2021-10-25 18:20:39 +0200

    Attempt at fixing pytorch dataloader

--- TrainCode/dataloader.py
+++ TrainCode/dataloader.py
@@ -100,8 +100,8 @@ class _MSDataLoaderIter(_SingleProcessDataLoaderIter):
                     maybe_device_id = None
                 self.worker_manager_thread = threading.Thread(
                     target=_pin_memory_loop,
-                    args=(self.worker_result_queue, self.data_queue, self.done_event, self.pin_memory,
-                          maybe_device_id))
+                    args=(self.worker_result_queue, self.data_queue, maybe_device_id, self.done_event)
+                )
                 self.worker_manager_thread.daemon = True
                 self.worker_manager_thread.start()
             else:
