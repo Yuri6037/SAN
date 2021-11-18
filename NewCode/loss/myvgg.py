34128f64b5ace64edea285924a57dce91d6e7d71
Yuri Edward <yuri6037@outlook.com>
2021-11-18 20:54:34 +0100

    Fixed type conversion issue

--- NewCode/loss/myvgg.py
+++ NewCode/loss/myvgg.py
@@ -1,5 +1,6 @@
 import torchvision
 import torch.nn as nn
+import torch
 
 from . import CX_distance
 
@@ -8,8 +9,13 @@ class MyVGG(nn.Module):
     def __init__(self):
         super(MyVGG, self).__init__()
         self.vgg = torchvision.models.vgg19(pretrained=True).features
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     def forward(self, sr, hr):
+        if torch.max(sr) > 1:
+            sr = torch.div(sr, 255.0) # normalize tensor if not already
+        hr = hr.to(self.device, dtype=torch.float32)
+        hr = torch.div(hr, 255.0) # normalize tensor
         sr_features = self.vgg(sr)
         hr_features = self.vgg(hr)
         loss = CX_distance.CX_loss(hr_features, sr_features)
